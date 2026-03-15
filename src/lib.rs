e42da17d19d0449924d7781b5372d99eedd85720
Alistair Israel <aisrael@gmail.com>
2026-03-15 17:01:42 -0400

    Factored out FileType

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,14 @@ pub const VERSION: &str = clap::crate_version!();
 
 pub mod cli;
 pub mod errors;
+pub mod file_type;
 pub mod pipeline;
 pub mod utils;
 
 pub use errors::Error;
-pub use utils::FileType;
+pub use file_type::FileType;
+pub use file_type::resolve_file_type;
 pub use utils::get_total_rows_result;
-pub use utils::resolve_file_type;
 
 /// Result type alias for datu operations.
 pub type Result<T> = std::result::Result<T, Error>;
